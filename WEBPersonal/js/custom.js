ccec4e1d890c46cca90a2e3dd05ce6d0b4e50af0
Alejandro Barahona Alvarez <alex.grajera@gmail.com>
2015-07-13 19:57:41 +0200

    Version 1.1

--- WEBPersonal/js/custom.js
+++ WEBPersonal/js/custom.js
@@ -6,6 +6,13 @@ $(document).onload(
     $('.tooltip-demo').tooltip({
         selector: "[data-toggle=tooltip]",
         container: "body"
-    })
-
+    }),
+    calcAge()
 );
+function calcAge() {
+    var birthday = new Date("July 24, 1991");
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    var age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    $("#age").html(age);
+}
