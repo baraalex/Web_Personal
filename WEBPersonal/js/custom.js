a3667bbdc84197a042f544d31a5c6533cdb47aab
Alejandro Barahona Alvarez <alex.grajera@gmail.com>
2015-07-13 16:09:29 +0200

    first push

---
+++ WEBPersonal/js/custom.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Alejandro on 11/07/2015.
+ */
+
+$(document).ready(function(){
+    $('.collapsible').collapsible({
+        accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
